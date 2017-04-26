5127051a1f7e15c2eb473037eb3dcc3d0343235c
sherlydevenoober <nghiadtse05330@fpt.edu.vn>
2017-04-26 14:07:33 +0700

    Update main.pas

--- main.pas
+++ main.pas
@@ -236,7 +236,33 @@ var
     injectString:Ansistring;
 begin
      //This thing to bypass the magic CSRF of portal
-   injectString:='__csrf_magic=sid%3A4d081935e8bb614e78a880650143293ee897aa04%2C1369301777&__csrf_magic=sid%3Ad7093ce3ac57317a23c34163c627506a7eb234f2%2C1369300478&__csrf_magic=sid%3A8d5d0afa62686d7cda3b2d1a9c8315ec894e6fa1%2C1369283024&__csrf_magic=sid%3A453e39dfc02d36d0fb2d60de1168e87dd7975423%2C1369273016&__csrf_magic=sid%3Aa6ba7557217602a4e1cbde0620188609080512ec%2C1369214375&__csrf_magic=sid%3Af1e3822e610ee4cb7649255b9435e368275a3611%2C1369211376&__csrf_magic=sid%3Ad028474708d22fdbed4b5e1c5987b66d4302d8c1%2C1369190175&__csrf_magic=sid%3A462dbf7995b1342e1a516aa4a0807c2fa048d6dd%2C1369131132&__csrf_magic=sid%3A30c1db18e28eb4c4b6b7a2b4be85107712c25647%2C1369043456&__csrf_magic=sid%3A82a31585ce588e6230b5c8a12f5e755497d74ef8%2C1369041021&__csrf_magic=sid%3A9652898b27ab4c916b5e8ed2d90c0eae43889ccc%2C1368438450&__csrf_magic=sid%3Ad14133c21cd4fb303647e25c1bfe2e7036fadc0a%2C1368415003&__csrf_magic=sid%3A9d73666ad6ad842423cc292094e97e5fb83b2ba8%2C1368169443&__csrf_magic=sid%3A7a3e553af6e88f92de4394ed870a07af5522e3d2%2C1368156691&__csrf_magic=sid%3Afbd81f19cab1ffedd3e512b1e7bbd190dd50ce3e%2C1368072090&__csrf_magic=sid%3A41c0a58dc1cdca183e17a3b8a2d1bd630f42a879%2C1364978702&__csrf_magic=sid%3A7465716204ec89c1e60cf98980d358666fb7fdf6%2C1363853126&__csrf_magic=sid%3A3d8c43a241925aee09ff44e791fd633f7250b747%2C1363770997&__csrf_magic=sid%3Af57485082db01c0ce1fd3af021f5c1a575b0ad11%2C1363603238&__csrf_magic=sid%3A077412139b4c4f126af45760a9563bc5707bf191%2C1362748829&__csrf_magic=sid%3A5ddbabe51849d39e579063135444d8056c84c533%2C1362746343&__csrf_magic=sid%3A2bd762876c168c2898a0c37f740abe035ad37993%2C1362732925&__csrf_magic=sid%3A53c486ec00bcc03b53cb3aa1844784708e4592bb%2C1362715048&__csrf_magic=sid%3Aa6ff345b691b2f2e091c2117bcb1ab03b2a51bb9%2C1349415940&__csrf_magic=sid%3A208acbaf72d72073e299445c84dd5024ea773674%2C1349414979&redirurl=&auth_user='+unametext+'&auth_pass='+pwordtext+'&accept=.';
+   
+   injectString:='__csrf_magic=sid%3A4d081935e8bb614e78a880650143293ee897aa04%2C1369301777&'
+   +'__csrf_magic=sid%3Ad7093ce3ac57317a23c34163c627506a7eb234f2%2C1369300478&'
+   +'__csrf_magic=sid%3A8d5d0afa62686d7cda3b2d1a9c8315ec894e6fa1%2C1369283024&'
+   +'__csrf_magic=sid%3A453e39dfc02d36d0fb2d60de1168e87dd7975423%2C1369273016&'
+   +'__csrf_magic=sid%3Aa6ba7557217602a4e1cbde0620188609080512ec%2C1369214375&'
+   +'__csrf_magic=sid%3Af1e3822e610ee4cb7649255b9435e368275a3611%2C1369211376&'
+   +'__csrf_magic=sid%3Ad028474708d22fdbed4b5e1c5987b66d4302d8c1%2C1369190175&'
+   +'__csrf_magic=sid%3A462dbf7995b1342e1a516aa4a0807c2fa048d6dd%2C1369131132&'
+   +'__csrf_magic=sid%3A30c1db18e28eb4c4b6b7a2b4be85107712c25647%2C1369043456&'
+   +'__csrf_magic=sid%3A82a31585ce588e6230b5c8a12f5e755497d74ef8%2C1369041021&'
+   +'__csrf_magic=sid%3A9652898b27ab4c916b5e8ed2d90c0eae43889ccc%2C1368438450&'
+   +'__csrf_magic=sid%3Ad14133c21cd4fb303647e25c1bfe2e7036fadc0a%2C1368415003&'
+   +'__csrf_magic=sid%3A9d73666ad6ad842423cc292094e97e5fb83b2ba8%2C1368169443&'
+   +'__csrf_magic=sid%3A7a3e553af6e88f92de4394ed870a07af5522e3d2%2C1368156691&'
+   +'__csrf_magic=sid%3Afbd81f19cab1ffedd3e512b1e7bbd190dd50ce3e%2C1368072090&'
+   +'__csrf_magic=sid%3A41c0a58dc1cdca183e17a3b8a2d1bd630f42a879%2C1364978702&'
+   +'__csrf_magic=sid%3A7465716204ec89c1e60cf98980d358666fb7fdf6%2C1363853126&'
+   +'__csrf_magic=sid%3A3d8c43a241925aee09ff44e791fd633f7250b747%2C1363770997&'
+   +'__csrf_magic=sid%3Af57485082db01c0ce1fd3af021f5c1a575b0ad11%2C1363603238&'
+   +'__csrf_magic=sid%3A077412139b4c4f126af45760a9563bc5707bf191%2C1362748829&'
+   +'__csrf_magic=sid%3A5ddbabe51849d39e579063135444d8056c84c533%2C1362746343&'
+   +'__csrf_magic=sid%3A2bd762876c168c2898a0c37f740abe035ad37993%2C1362732925&'
+   +'__csrf_magic=sid%3A53c486ec00bcc03b53cb3aa1844784708e4592bb%2C1362715048&'
+   +'__csrf_magic=sid%3Aa6ff345b691b2f2e091c2117bcb1ab03b2a51bb9%2C1349415940&'
+   +'__csrf_magic=sid%3A208acbaf72d72073e299445c84dd5024ea773674%2C1349414979&'
+   +'redirurl=&auth_user='+unametext+'&auth_pass='+pwordtext+'&accept=.';
 
    result:=HttpPostURL(portal,injectString,data);
 end;
@@ -250,8 +276,11 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-     //creaft Appdata folder
+     //create Appdata folder
      getAppDatapath;
+
+     //it can be set in Object Inspector but i put this in here, for sure.
+     timer1.Enabled:=false;
 
      AppDataPath:=AppDataPath+'\fptudomlogger';
      if not DirectoryExists(AppDataPath) then
@@ -341,7 +370,11 @@ begin
 
                 //Show message
                 Showmessage(label1.caption);
+
+                //Start Automatic mode
+                timer1.Enabled:=true;
                 form1.hide;
+
            end;
      end
   else
@@ -365,4 +398,3 @@ begin
 end;
 
 end.
-
